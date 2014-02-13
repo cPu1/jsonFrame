473fa9da936ca8e9ab21538fe5650f97ac94b3f2
cPu1 <patwal.chetan@gmail.com>
2014-02-13 12:19:27 +0530

    Update valid-rpc.js

--- test/valid-rpc.js
+++ test/valid-rpc.js
@@ -1,5 +1,43 @@
 var jFrame = require('../lib/jsonFrame'),
 jsonFrame = jFrame({lengthPrefix: 2}),
-rpcServer = jsonFrame.server({host: 'localhost', port: 3000});
+jsonTransformer = jsonFrame.jsonTransformer(),
+net = require('net'),
+options = {host: 'localhost', port: 3000},
+rpcServer = jsonFrame.server(options),
+should = require('should'),
+assert = require('assert'),
 
 
+socket = net.connect(options);
+socket.pipe(jsonTransformer);
+
+describe('JsonRpc validation', function () {
+  var malformedRequests = [ {
+    asdf: 'what'
+  }, {
+    params: []
+  },
+  {
+    params: [1]
+  }, {
+    id: 420
+  }
+    ];
+    
+  
+  it('should keep receiving errors', function (done) {
+    var noOfRequests = 0;
+    jsonTransformer.on('data', function (json) {
+      json.should.have.properties('error', 'id');
+      json.error.should.have.properties('code', 'message');
+      json.error.code.should.equal(-32600);
+      json.error.message.should.equal('Invalid Request');
+      assert.equal(json.id, null);
+      if(++noOfRequests >= malformedRequests.length) done();
+    });
+  });
+  
+  malformedRequests.forEach(function (req) {
+      socket.write(jsonFrame.build(req));
+  });
+});
